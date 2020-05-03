775c11dcd7e79f3937c8087aed00cb4a673edf29
Billy Keyes <bluekeyes@gmail.com>
2020-05-02 17:13:11 -0700

    Fix lint errors in ParseRepository

--- repository.go
+++ repository.go
@@ -20,9 +20,8 @@ func (r Repository) String() string {
 
 // ParseRepository parses a Repository from a string in "owner/name" format.
 func ParseRepository(s string) (Repository, error) {
-	if i := strings.IndexByte(s, '/'); i < 1 || i == len(s)-1 {
-		return Repository{}, fmt.Errorf("invalid repository: %s", s)
-	} else {
+	if i := strings.IndexByte(s, '/'); 1 <= i && i < len(s)-1 {
 		return Repository{Owner: s[:i], Name: s[i+1:]}, nil
 	}
+	return Repository{}, fmt.Errorf("invalid repository: %s", s)
 }
