72e844a14e77e3964ad488e3249057c2bd6a9729
Billy Keyes <bluekeyes@gmail.com>
2022-02-20 20:43:24 -0800

    Update google/go-github to v42

--- cmd/patch2pr/main.go
+++ cmd/patch2pr/main.go
@@ -16,7 +16,7 @@ import (
 
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
 	"github.com/bluekeyes/patch2pr"
-	"github.com/google/go-github/v41/github"
+	"github.com/google/go-github/v42/github"
 	"golang.org/x/oauth2"
 )
 
