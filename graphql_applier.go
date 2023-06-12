3ec592b87f2fd2d06b7eac2120f8538e498d55e0
Billy Keyes <bluekeyes@gmail.com>
2023-06-12 09:09:49 -0700

    Update google/go-github to v53

--- graphql_applier.go
+++ graphql_applier.go
@@ -11,7 +11,7 @@ import (
 	"path"
 
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
-	"github.com/google/go-github/v52/github"
+	"github.com/google/go-github/v53/github"
 	"github.com/shurcooL/githubv4"
 )
 
--- reference.go
+++ reference.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/google/go-github/v52/github"
+	"github.com/google/go-github/v53/github"
 )
 
 // Reference is a named reference in a repository.
