ca743b151da202f411bbbb789f2da927005a9f67
Billy Keyes <bluekeyes@gmail.com>
2023-06-12 12:12:18 -0400

    Update google/go-github to v53 (#54)

--- cmd/patch2pr/main.go
+++ cmd/patch2pr/main.go
@@ -16,7 +16,7 @@ import (
 
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
 	"github.com/bluekeyes/patch2pr"
-	"github.com/google/go-github/v52/github"
+	"github.com/google/go-github/v53/github"
 	"golang.org/x/oauth2"
 )
 
