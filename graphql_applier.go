002ea2c0493aa4f297627661601dbc8adc127baf
Billy Keyes <bluekeyes@gmail.com>
2023-10-14 18:51:19 -0400

    Update google/go-github to v56 (#66)

--- graphql_applier.go
+++ graphql_applier.go
@@ -11,7 +11,7 @@ import (
 	"path"
 
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
-	"github.com/google/go-github/v55/github"
+	"github.com/google/go-github/v56/github"
 	"github.com/shurcooL/githubv4"
 )
 
