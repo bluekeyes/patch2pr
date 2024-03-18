2a1011ff2e060e1a1cdb56e1737eb2897d96ad0a
Billy Keyes <commits@billykeyes.dev>
2024-03-18 13:38:04 -0700

    Update google/go-github to v60

--- cmd/patch2pr/main.go
+++ cmd/patch2pr/main.go
@@ -17,7 +17,7 @@ import (
 
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
 	"github.com/bluekeyes/patch2pr"
-	"github.com/google/go-github/v59/github"
+	"github.com/google/go-github/v60/github"
 	"golang.org/x/oauth2"
 )
 
