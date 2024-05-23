a6cc2fb27dcf20ac32034b80ea8bfca2207ecc80
Andrew Svoboda <svoboda.andrew@gmail.com>
2024-05-23 18:13:50 +0100

    Update google/go-github to v62 (#103)

--- graphql_applier.go
+++ graphql_applier.go
@@ -11,7 +11,7 @@ import (
 	"path"
 
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
-	"github.com/google/go-github/v61/github"
+	"github.com/google/go-github/v62/github"
 	"github.com/shurcooL/githubv4"
 )
 
