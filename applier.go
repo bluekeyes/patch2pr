621ae05ef4196d346cdc5c5217097d50933a3481
Billy Keyes <commits@billykeyes.dev>
2024-09-14 13:59:07 -0400

    Update google/go-github to v64 (#111)

--- applier.go
+++ applier.go
@@ -13,7 +13,7 @@ import (
 	"time"
 
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
-	"github.com/google/go-github/v63/github"
+	"github.com/google/go-github/v64/github"
 )
 
 // DefaultCommitMessage is the commit message used when no message is provided
