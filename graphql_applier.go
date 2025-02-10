782edd349bbfd67d33eb1e5f2c10e7d1330640c8
Andrew Svoboda <asvoboda@palantir.com>
2025-02-10 17:18:18 +0000

    Update google/go-github to v69 (#121)

--- graphql_applier.go
+++ graphql_applier.go
@@ -11,7 +11,7 @@ import (
 	"path"
 
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
-	"github.com/google/go-github/v68/github"
+	"github.com/google/go-github/v69/github"
 	"github.com/shurcooL/githubv4"
 )
 
