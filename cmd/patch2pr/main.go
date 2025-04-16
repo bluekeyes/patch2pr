5b8d0125be2a7aae2cdc58d96690be0097a77256
Billy Keyes <commits@billykeyes.dev>
2025-04-15 22:10:37 -0700

    Update google/go-github to v71 (#133)

--- cmd/patch2pr/main.go
+++ cmd/patch2pr/main.go
@@ -16,7 +16,7 @@ import (
 	"time"
 
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
-	"github.com/google/go-github/v70/github"
+	"github.com/google/go-github/v71/github"
 
 	"github.com/bluekeyes/patch2pr"
 	"github.com/bluekeyes/patch2pr/internal"
--- reference.go
+++ reference.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/google/go-github/v70/github"
+	"github.com/google/go-github/v71/github"
 )
 
 // Reference is a named reference in a repository.
