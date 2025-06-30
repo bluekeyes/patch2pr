1e82e8754be50377d09aa24ab0273854602cd5b6
Andrew Svoboda <asvoboda@palantir.com>
2025-06-30 17:22:33 +0100

    Update go-github to v73 (#137)

--- cmd/patch2pr/main.go
+++ cmd/patch2pr/main.go
@@ -16,7 +16,7 @@ import (
 	"time"
 
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
-	"github.com/google/go-github/v72/github"
+	"github.com/google/go-github/v73/github"
 
 	"github.com/bluekeyes/patch2pr"
 	"github.com/bluekeyes/patch2pr/internal"
