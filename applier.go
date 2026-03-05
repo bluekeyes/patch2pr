78d9a95491f773d5e1db7d961c27334000c6d828
Billy Keyes <commits@billykeyes.dev>
2026-03-05 09:13:04 -0500

    Update google/go-github to v84
    Update the minimum module Go version to 1.25.0 as required by go-github.
    Also update golangci-lint for Go 1.25.0 compatibility.

--- applier.go
+++ applier.go
@@ -13,7 +13,7 @@ import (
 	"time"
 
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
-	"github.com/google/go-github/v83/github"
+	"github.com/google/go-github/v84/github"
 )
 
 // DefaultCommitMessage is the commit message used when no message is provided
